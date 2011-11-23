d945fb0b783c92dd85028497305c32f95d969a39
daoo <daoo314@gmail.com>
2011-11-23 22:30:38 +0100

    Clean up

--- interpreters/haskell/Interpreter.hs
+++ interpreters/haskell/Interpreter.hs
@@ -1,7 +1,8 @@
-module Interpreter (brainfuck) where
+module Interpreter (brainfuck, brainfuckIO) where
 
 import Debug.Trace
 
+import Data.Word
 import Data.Char
 
 import Ext
@@ -10,22 +11,42 @@ brainfuckChars :: [Char]
 brainfuckChars = "-+<>[].,"
 
 brainfuck :: String -> String -> String
-brainfuck str inp = map chr $ interpret (("", str'), (zeros, zeros)) inp'
+brainfuck str inp = map (chr . fromIntegral) $ interpret (newState str) inp'
   where
-    str'  = filter (flip elem brainfuckChars) str
-    inp'  = map ord $ inp ++ "\NUL"
+    str' = filter (flip elem brainfuckChars) str
+    inp' = map ordCell $ inp ++ "\NUL"
+
+brainfuckIO :: String -> IO ()
+brainfuckIO str = interpretIO (newState str)
+
+newState :: String -> State
+newState str = (("", str'), (zeros, zeros)) 
+  where
+    str' = filter (flip elem brainfuckChars) str
     zeros = iterate id 0
 
-type Cell   = Int
+type Cell   = Word8
 type Memory = ([Cell], [Cell])
 type Code   = (String, String)
 type State  = (Code, Memory)
 
--- interpretIO
+chrCell :: Cell -> Char
+chrCell = chr . fromIntegral
+
+ordCell :: Char -> Cell
+ordCell = fromIntegral . ord
+
+interpretIO :: State -> IO ()
+interpretIO ((_, []), _)   = return ()
+interpretIO state@(str, m) =
+  case str of
+    (_,'.':_) -> putChar (chrCell $ current m) >> interpretIO (shiftL str, m)
+    (_,',':_) -> getChar >>= (\x -> interpretIO (shiftL str, modify (const $ ordCell x) m))
+    _ -> interpretIO (nonIO state)
 
 interpret :: State -> [Cell] -> [Cell]
 interpret ((_, []), _) _     = []
-interpret state@(str, m) inp = trace (show (mapBoth (takeWhile ( /= 0)) m)) $
+interpret state@(str, m) inp = -- trace (show (mapBoth (takeWhile ( /= 0)) m)) $
   case str of
     (_,'.':_) -> current m : interpret (shiftL str, m) inp
     (_,',':_) -> case inp of
@@ -34,6 +55,7 @@ interpret state@(str, m) inp = trace (show (mapBoth (takeWhile ( /= 0)) m)) $
                    (x:xs) -> interpret (shiftL str, modify (const x) m) xs
     _ -> interpret (nonIO state) inp
 
+nonIO :: State -> State
 nonIO (s, m) =
   case s of
     (_,'>':_) -> (shiftL s, shiftL m)
