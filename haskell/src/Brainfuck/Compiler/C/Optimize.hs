36bae0c3cfae28671075db5661b1db716a16466f
daoo <daoo314@gmail.com>
2012-05-06 19:22:11 +0200

    Tweak optimization order

--- haskell/src/Brainfuck/Compiler/C/Optimize.hs
+++ haskell/src/Brainfuck/Compiler/C/Optimize.hs
@@ -7,10 +7,10 @@ import Brainfuck.Ext
 optimize :: [IL] -> [IL]
 optimize = merge1 reduceLoops
          . filterIL clean
-         . merge2 mergeSame
          . merge2 applyShifts
          . merge2 sortMutators
          . whileModified (merge2 shiftShifts)
+         . whileModified (merge2 mergeSame)
 
 optimizeForC :: [IL] -> [IL]
 optimizeForC = removeFromEnd . whileModified optimize
