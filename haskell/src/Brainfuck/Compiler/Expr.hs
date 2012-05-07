e8859d553d42a3dba6a55617de8e24961b64895b
daoo <daoo314@gmail.com>
2012-05-07 09:19:06 +0200

    Misc clean up

--- haskell/src/Brainfuck/Compiler/Expr.hs
+++ haskell/src/Brainfuck/Compiler/Expr.hs
@@ -25,4 +25,5 @@ cleanExpr expr = case expr of
   Mult (Const 1) e -> cleanExpr e
   Plus e1 e2       -> Plus (cleanExpr e1) (cleanExpr e2)
   Mult e1 e2       -> Mult (cleanExpr e1) (cleanExpr e2)
-  e                -> e
+
+  e -> e
