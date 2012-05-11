64dc4c69822d7de506bfa061dda4b76ac6d91ef4
daoo <daoo314@gmail.com>
2012-05-11 16:54:22 +0200

    Rewrite showC
      * Use difference list
      * Also don't allocate any memory if it's not needed

--- haskell/src/Brainfuck/Compiler/Target/C99.hs
+++ haskell/src/Brainfuck/Compiler/Target/C99.hs
@@ -1,5 +1,6 @@
 module Brainfuck.Compiler.Target.C99 (showC, optimizeForC) where
 
+import Brainfuck.Compiler.Analyzer
 import Brainfuck.Compiler.Expr
 import Brainfuck.Compiler.IL
 import Brainfuck.Compiler.Optimize
@@ -17,57 +18,58 @@ optimizeForC = removeFromEnd . times optimize 100
              . mapIL optimizeExpressions
              . applyIL
 
+showExpr :: Expr -> ShowS
+showExpr (Const c)            = shows c
+showExpr (Get d)              = showString "ptr[" . shows d . showString "]"
+showExpr (Add e1 e2)          = showExpr e1 . showString " + " . showExpr e2
+showExpr (Mul (Add e1 e2) e3) = showString "(" . showExpr e1 . showString " + " . showExpr e2 . showString ") * " . showExpr e3
+showExpr (Mul e1 (Add e2 e3)) = showExpr e1 . showString " * (" . showExpr e2 . showString " + " . showExpr e3 . showString ")"
+showExpr (Mul e1 e2)          = showExpr e1 . showString " * " . showExpr e2
+
+indent :: Int -> ShowS
+indent i = (replicate (2 * i) ' ' ++)
+
 showC :: [IL] -> String
-showC ils = program mem $ toC 1 ils
+showC ils = unlines $ begin $ mem ils $ code 1 ils $ newLine end
   where
-    mem = 30001
+    defaultMem :: Int
+    defaultMem = 30001
 
-showExpr :: Expr -> String
-showExpr (Const c)            = show c
-showExpr (Get d)              = "ptr[" ++ show d ++ "]"
-showExpr (Add e1 e2)          = showExpr e1 ++ " + " ++ showExpr e2
-showExpr (Mul (Add e1 e2) e3) = "(" ++ showExpr e1 ++ " + " ++ showExpr e2 ++ ") * " ++ showExpr e3
-showExpr (Mul e1 (Add e2 e3)) = showExpr e1 ++ " * (" ++ showExpr e2 ++ " + " ++ showExpr e3 ++ ")"
-showExpr (Mul e1 e2)          = showExpr e1 ++ " * " ++ showExpr e2
+    newLine :: [String] -> [String]
+    newLine = ("" :)
 
-program :: Int -> String -> String
-program mem code =
-  unlines [ "#include <stdio.h>"
-          , ""
-          , "int main() {"
-          , indent 1 ++ "char mem[" ++ show mem ++ "];"
-          , indent 1 ++ "char *ptr = mem;"
-          , ""
-          , code
-          , "  return 0;"
-          , "}" ]
+    begin :: [String] -> [String]
+    begin prep = "#include <stdio.h>" : ("" : ("int main() {" : prep))
 
-indent :: Int -> String
-indent i = replicate (2 * i) ' '
+    end :: [String]
+    end = [ indent 1 "return 0;", "}" ]
+          
+    mem xs prep = if usesMemory xs
+      then alloc $ ptr $ newLine prep
+      else prep
+      where
+        alloc = (:) (indent 1 $ showString "char mem[" $ shows defaultMem "];")
+        ptr   = (:) (indent 1 "char* ptr = mem;")
 
-toC :: Int -> [IL] -> String
-toC = helper
-  where
-    helper _ []              = []
-    helper i (Loop d l : xs) = concat [ indent i
-                                      , "while (ptr["
-                                      , show d
-                                      , "]) {\n"
-                                      , helper (i + 1) l
-                                      , indent i
-                                      , "}\n"
-                                      , helper i xs ]
-    helper i (x : xs) = concat [ indent i
-                               , line x
-                               , "\n"
-                               , helper i xs ]
+    code _ [] prep                 = prep
+    code i (Loop d loop : xs) prep = while $ body $ close $ code i xs prep
+      where
+        while = (:) (indent i $ showString "while (ptr[" $ shows d "]) {")
+        close = (:) (indent i "}")
+        body  = code (i + 1) loop
 
-    line (Set d1 (Get d2 `Add` Const c)) | d1 == d2 = concat [ "ptr[", show d1, "] += ", show c, ";" ]
-    line (Set d1 (Const c `Add` Get d2)) | d1 == d2 = concat [ "ptr[", show d1, "] += ", show c, ";" ]
+    code i (x : xs) prep = indent i (line x) : code i xs prep
+      
+    line x = case x of
+      Set d1 (Get d2 `Add` Const c) | d1 == d2 -> ptr (shows d1) "+=" (shows c)
+      Set d1 (Const c `Add` Get d2) | d1 == d2 -> ptr (shows d1) "+=" (shows c)
 
-    line (Set d e)   = concat [ "ptr[", show d, "] = ", showExpr e, ";" ]
-    line (Shift s)   = concat [ "ptr += ", show s, ";" ]
-    line (PutChar e) = concat [ "putchar(", showExpr e, ");" ]
-    line (GetChar p) = concat [ "ptr[", show p, "] = getchar();" ]
+      Set d e   -> ptr (shows d) "=" (showExpr e)
+      Shift s   -> showString "ptr += " $ shows s ";"
+      PutChar e -> showString "putchar(" $ showExpr e ");"
+      GetChar p -> ptr (shows p) "=" (showString "getchar()")
 
-    line (Loop _ _) = error "error"
+      Loop _ _ -> error "Should not happen"
+
+      where
+        ptr a op b = showString "ptr[" $ a $ showString "] " $ showString op $ showString " " $ b ";"
