e4ccbafe96060aa992808a8341d46a05ab34a4c7
daoo <daoo314@gmail.com>
2012-05-11 14:42:20 +0200

    Misc clean up

--- haskell/src/Brainfuck/Compiler/Brainfuck.hs
+++ haskell/src/Brainfuck/Compiler/Brainfuck.hs
@@ -1,6 +1,6 @@
 module Brainfuck.Compiler.Brainfuck (compile) where
 
-import qualified Brainfuck.Compiler.Expr as Expr
+import Brainfuck.Compiler.Expr
 import Brainfuck.Compiler.IL
 import Brainfuck.Parser.Brainfuck
 
@@ -10,9 +10,9 @@ compile (Repeat l : bs)  = Loop 0 (compile l) : compile bs
 compile (Token tok : bs) = tok' : compile bs
   where
     tok' = case tok of
-      Plus       -> Set 0 $ Expr.Add (Expr.Get 0) (Expr.Const 1)
-      Minus      -> Set 0 $ Expr.Add (Expr.Get 0) (Expr.Const (-1))
+      Plus       -> Set 0 $ Add (Get 0) (Const 1)
+      Minus      -> Set 0 $ Add (Get 0) (Const (-1))
       ShiftRight -> Shift 1
       ShiftLeft  -> Shift (-1)
-      Output     -> PutChar $ Expr.Get 0
+      Output     -> PutChar $ Get 0
       Input      -> GetChar 0
