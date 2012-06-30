01c660e4456b1dd9a4d44c9516c5740a74bdf281
daoo <daoo314@gmail.com>
2012-06-30 12:54:43 +0200

    Rewrite main
    It's still somewhat messy

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,58 +1,65 @@
 module Main where
 
 import Data.Foldable (toList)
-import Data.List
-import Data.Word
 
+import System.Console.GetOpt
 import System.Environment
-import System.Directory
 
 import Brainfuck.Compiler.Brainfuck
 import Brainfuck.Compiler.IL
 import Brainfuck.Compiler.Optimize
 import Brainfuck.Compiler.Target.C99
+import Brainfuck.Compiler.Target.Indented
+import Brainfuck.Ext
 import Brainfuck.Interpreter.Interpreter
 import Brainfuck.Interpreter.State
 import Brainfuck.Parser.Parser
 
-data Action = ToC | Interpret
+data Action = Compile | Interpret deriving (Show, Read)
+data Target = Indented | C99 deriving (Show, Read)
 
-params :: [String] -> IO (Action, String)
-params p = code (concat p') >>= (\bf -> return (action, bf))
-  where
-    (pc, p') = partition (== "-c") p
-    action = case pc of
-      []     -> Interpret
-      ["-c"] -> ToC
-      _      -> error "Unrecognized parameter"
+data Options = Options
+  { optAction :: Action
+  , optTarget :: Target
+  }
 
-    code str = do
-      isfile <- doesFileExist str
-      if isfile
-        then readFile str
-        else return str
+defaultOptions :: Options
+defaultOptions = Options
+  { optAction = Interpret
+  , optTarget = C99
+  }
+
+data Flag = Do Action | Target Target
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option "c" ["compile"] (NoArg (\opt -> opt { optAction = Compile })) "compile input"
+  , Option "t" ["target"] (ReqArg (\arg opt -> opt { optTarget = read arg })  "C99 | Indented") "target language"
+  ]
 
 main :: IO ()
 main = do
-  argv         <- getArgs
-  (what, code) <- params argv
-  inp          <- getContents
+  args <- getArgs
+  (flags, file) <- case getOpt RequireOrder options args of
+    (flags , nonOpts , [])   -> return (flags, concat nonOpts)
+    (_     , _       , msgs) -> error $ concat msgs
 
-  il <- case parseBrainfuck code of
+  let opts = pipe flags defaultOptions
+
+  code <- readFile file
+
+  ils <- case parseBrainfuck code of
     Left err -> error $ show err
     Right bf -> return $ compile bf
 
-  putStrLn $ case what of
-    ToC       -> makeC il
-    Interpret -> runBF inp il
+  let optimized = optimizeAll ils
+
+  case optAction opts of
+    Compile    -> case optTarget opts of
+      C99      -> putStrLn $ showC optimized
+      Indented -> putStrLn $ showIndented optimized
+    Interpret  -> getContents >>= putStrLn . (`runBF` optimized)
 
   where
-    makeC :: [IL] -> String
-    makeC = showC . optimizeAll
-
     runBF :: String -> [IL] -> String
-    runBF inp = map chrIntegral . toList . getOutput . run state
-      where
-        state :: State Word8
-        state = newState inp
-
+    runBF inp = map chrIntegral . toList . getOutput . run (newState inp)
