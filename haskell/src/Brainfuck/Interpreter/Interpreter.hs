08a49550e4b117313df39855ade3062a6a642e34
daoo <daoo314@gmail.com>
2012-06-30 11:27:00 +0200

    Fix list zipper peek and applyAt

--- haskell/src/Brainfuck/Interpreter/Interpreter.hs
+++ haskell/src/Brainfuck/Interpreter/Interpreter.hs
@@ -5,7 +5,6 @@ import Data.ListZipper
 
 import Brainfuck.Compiler.Expr
 import Brainfuck.Compiler.IL
-import Brainfuck.Ext
 import Brainfuck.Interpreter.State
 
 run :: (Integral a) => State a -> [IL] -> State a
@@ -17,21 +16,16 @@ evalOp state@(State inp out mem) x = case x of
   If e ys    -> if 0 == evalExpr e (`peek` mem) then run state ys else state
 
   PutChar e -> State inp        (out |> evalExpr' e) mem
-  GetChar d -> State (tail inp) out                  (applyIndex' (head inp) d mem)
-  Set d e   -> State inp        out                  (applyIndex' (evalExpr' e) d mem)
+  GetChar d -> State (tail inp) out                  (applyAt' (head inp) d mem)
+  Set d e   -> State inp        out                  (applyAt' (evalExpr' e) d mem)
   Shift s   -> State inp        out                  (move s mem)
 
   where
     evalExpr' e = evalExpr e (`peek` mem)
-    applyIndex' = applyIndex . const
+    applyAt' = applyAt . const
 
 evalExpr :: (Integral a) => Expr -> (Int -> a) -> a
 evalExpr (Const v) _   = fromIntegral v
 evalExpr (Get o) f     = f o
 evalExpr (Add e1 e2) f = evalExpr e1 f + evalExpr e2 f
 evalExpr (Mul e1 e2) f = evalExpr e1 f * evalExpr e2 f
-
-applyIndex :: (a -> a) -> Int -> ListZipper a -> ListZipper a
-applyIndex f 0 (ListZipper xs y zz)             = ListZipper xs (f y) zz
-applyIndex f n (ListZipper xs y zz) | n < 0     = ListZipper (mapIndex f (abs n - 1) xs) y zz
-                                    | otherwise = ListZipper xs y (mapIndex f (n - 1) zz)
--- haskell/src/Data/ListZipper.hs
+++ haskell/src/Data/ListZipper.hs
@@ -1,5 +1,6 @@
 module Data.ListZipper where
 
+import Brainfuck.Ext
 import Test.QuickCheck.Arbitrary
 
 data ListZipper a = ListZipper
@@ -24,11 +25,16 @@ move   n  lz | n < 0                 = move (n + 1) (move (-1) lz)
 
 peek :: Int -> ListZipper a -> a
 peek 0 lz             = focus lz
-peek n lz | n < 0     = left lz !! abs n
-          | otherwise = right lz !! n
+peek n lz | n < 0     = left lz !! (abs n - 1)
+          | otherwise = right lz !! (n - 1)
 
 apply :: (a -> a) -> ListZipper a -> ListZipper a
 apply f (ListZipper xs y zs) = ListZipper xs (f y) zs
 
 cut :: Int -> ListZipper a -> [a]
 cut i (ListZipper xs y zs) = take i xs ++ [y] ++ take i zs
+
+applyAt :: (a -> a) -> Int -> ListZipper a -> ListZipper a
+applyAt f 0 (ListZipper xs y zz)             = ListZipper xs (f y) zz
+applyAt f n (ListZipper xs y zz) | n < 0     = ListZipper (mapIndex f (abs n - 1) xs) y zz
+                                 | otherwise = ListZipper xs y (mapIndex f (n - 1) zz)
