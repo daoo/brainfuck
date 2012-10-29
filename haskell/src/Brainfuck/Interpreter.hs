b288f627b790b521a708c5d46ce5e4af9564f51e
daoo <daoo314@gmail.com>
2012-10-29 14:57:53 +0100

    Formatting

--- haskell/src/Brainfuck/Interpreter.hs
+++ haskell/src/Brainfuck/Interpreter.hs
@@ -7,11 +7,11 @@ import Data.Char
 import Data.ListZipper
 import Data.Sequence
 
-data State a = State {
-  getInput :: [a],
-  getOutput :: Seq a,
-  getMemory :: ListZipper a
-} deriving (Show, Eq)
+data State a = State
+  { getInput :: [a]
+  , getOutput :: Seq a
+  , getMemory :: ListZipper a
+  } deriving (Show, Eq)
 
 chrIntegral :: (Integral a) => a -> Char
 chrIntegral = chr . fromIntegral
@@ -20,7 +20,8 @@ ordIntegral :: (Integral a) => Char -> a
 ordIntegral = fromIntegral . ord
 
 newMemory :: (Integral a) => ListZipper a
-newMemory = let zeros = repeat 0 in ListZipper zeros 0 zeros
+newMemory = ListZipper zeros 0 zeros
+  where zeros = repeat 0
 
 newState :: (Integral a) => String -> State a
 newState inp = State (map ordIntegral inp) empty newMemory
