9f464cfc0e3cf2f625c266be7f73f88fe90217c6
daoo <daoo314@gmail.com>
2012-11-29 20:43:16 +0100

    Fix analysis

--- haskell/src/Brainfuck/Compiler/Analysis.hs
+++ haskell/src/Brainfuck/Compiler/Analysis.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE LambdaCase #-}
 module Brainfuck.Compiler.Analysis where
 
+import Brainfuck.Data.AST
 import Brainfuck.Data.Expr
-import Brainfuck.Data.IL
+import Control.Applicative ((<$>), (<|>))
 import Data.List
 import Data.Maybe
 
@@ -20,18 +21,28 @@ exprDepends d = unfold (||) (||) f
 --     must be decremented by 1, otherwise we do not know if it will reach zero.
 --   * Increment or decrement any other memory cell by any integer.
 -- If the supplied instruction isn't a Loop, we will return Nothing.
-copyLoop :: Int -> [IL] -> Maybe [(Int, Int)]
+copyLoop :: Int -> AST -> Maybe [(Int, Int)]
 copyLoop d xs = do
-  sets <- mapM f xs
-  let (dec, copies) = partition (g d) sets
+  instrs <- instructionsOnly xs
+  sets <- mapM setsOnly instrs
+  adds <- mapM constantAddOnly sets
+  let (dec, copies) = partition (g d) adds
   listToMaybe dec
   mapM h copies
   where
-    -- First filter, if a non-constant add is found, exit
-    f = \case
-      Set d1 (Get d2 `Add` Const c) -> Just (d1, d2, c)
-      Set d1 (Const c `Add` Get d2) -> Just (d1, d2, c)
-      _                             -> Nothing
+    instructionsOnly = \case
+      Nop                  -> Just []
+      Instruction fun next -> (fun :) <$> instructionsOnly next
+      Flow _ _ _           -> Nothing
+
+    setsOnly = \case
+      Set d' e -> Just (d', e)
+      _        -> Nothing
+
+    constantAddOnly = \case
+      (d1, Get d2 `Add` Const c) -> Just (d1, d2, c)
+      (d1, Const c `Add` Get d2) -> Just (d1, d2, c)
+      _                          -> Nothing
 
     -- Filter the decrement operation
     g d1 (d2, d3, -1) = d1 == d2 && d1 == d3
@@ -40,52 +51,32 @@ copyLoop d xs = do
     h (d1, d2, c) | d1 == d2  = Just (d1, c)
                   | otherwise = Nothing
 
-memoryAccess :: [IL] -> [[Int]]
-memoryAccess = go 0
-  where
-    go _ []     = []
-    go i (x:xs) = case x of
-      If _ _    -> error "FIXME: memoryAccess If"
-      While _ _ -> error "FIXME: memoryAccess While"
-      Shift s   -> go (i + s) xs
-      Set d e   -> ((i + d) : expr i e) : go i xs
-      PutChar e -> expr i e             : go i xs
-      GetChar d -> [i + d]              : go i xs
-
-    expr i = unfold (++) (++) f
-      where
-        f (Get d) = [i + d]
-        f _       = []
-
--- |Approximate how much memory is needed
-memorySize :: [IL] -> (Int, Int)
-memorySize xs = case concat $ memoryAccess xs of
-  []  -> (0, 0)
-  xs' -> (minimum xs', maximum xs')
-
 -- |Check if the list of ILs make use of the memory or the global pointer
-usesMemory :: [IL] -> Bool
-usesMemory = any $ \case
-  If _ ys    -> usesMemory ys
-  While _ ys -> usesMemory ys
-  PutChar e  -> unfold (||) (||) g e
-  Set _ _    -> True
-  GetChar _  -> True
-  Shift _    -> True
+usesMemory :: AST -> Bool
+usesMemory = \case
+  Nop                  -> False
+  Instruction fun next -> f fun || usesMemory next
+  Flow _ inner next    -> usesMemory inner || usesMemory next
+
   where
+    f = \case
+      PutChar e -> unfold (||) (||) g e
+      Set _ _   -> True
+      GetChar _ -> True
+      Shift _   -> True
+
     g (Get _) = True
     g _       = False
 
 -- |Check if a memory position is set to zero by some ILs
-setToZero :: Int -> [IL] -> Maybe [IL]
-setToZero d1 = fmap reverse . go . reverse
+setToZero :: Int -> AST -> Bool
+setToZero d1 ast = maybe False (== 0) (go Nothing ast)
   where
-    go []       = Just []
-    go (x : xs) = case x of
-      If _ _                      -> Nothing
-      While _ _                   -> Nothing
-      Shift _                     -> Nothing
-      GetChar d2       | d1 == d2 -> Nothing
-      Set d2 (Const 0) | d1 == d2 -> Just xs
-      Set d2 _         | d1 == d2 -> Nothing
-      _                           -> fmap (x :) $ go xs
+    go t = \case
+      Nop                  -> t
+      Instruction fun next -> go (f fun <|> t) next
+      Flow _ _ _           -> Nothing
+
+    f = \case
+      Set d2 (Const i) | d1 == d2 -> Just i
+      _                           -> Nothing
