212d49c03b9029d23bb7ade4353de2b828a47ad8
daoo <daoo314@gmail.com>
2012-11-30 20:38:04 +0100

    Implement Arbitrary for AST

--- haskell/src/Brainfuck/Data/AST.hs
+++ haskell/src/Brainfuck/Data/AST.hs
@@ -2,7 +2,7 @@
 module Brainfuck.Data.AST where
 
 import Brainfuck.Data.Expr
-import Control.Monad (liftM, liftM2)
+import Control.Applicative
 import Test.QuickCheck
 
 data Function = Set Int Expr | Shift Int | PutChar Expr | GetChar Int
@@ -20,15 +20,30 @@ data AST = Nop
   deriving (Eq, Show)
 
 instance Arbitrary Function where
-  arbitrary = frequency [ (4, liftM2 Set (choose (-4, 10)) arbitrary)
-                        , (2, liftM Shift (choose (-4, 10)))
-                        , (1, liftM PutChar arbitrary)
+  arbitrary = frequency [ (4, liftA2 Set (choose (-4, 10)) arbitrary)
+                        , (2, liftA Shift (choose (-4, 10)))
+                        , (1, liftA PutChar arbitrary)
                         ]
   shrink = \case
     Set i e   -> map (Set i) $ shrink e
     Shift i   -> map Shift $ shrink i
     PutChar e -> map PutChar $ shrink e
     GetChar i -> map GetChar $ shrink i
+
+instance Arbitrary Control where
+  arbitrary = oneof [ return Once
+                    , return Never
+                    , liftA If arbitrary
+                    ]
+
+instance Arbitrary AST where
+  arbitrary = sized tree
+    where
+      tree 0 = return Nop
+      tree n = frequency [ (4, liftA2 Instruction arbitrary subtree)
+                         , (1, liftA3 Flow arbitrary subtree subtree) ]
+        where
+          subtree = tree (n `div` 2)
 
 join :: AST -> AST -> AST
 join a b = case a of
