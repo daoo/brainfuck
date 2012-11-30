6e5c9c1fcb9a8b358950b352c73db7a83604fdf6
daoo <daoo314@gmail.com>
2012-11-30 22:07:30 +0100

    Remove broken test

--- haskell/tests/Properties/Expressions.hs
+++ haskell/tests/Properties/Expressions.hs
@@ -31,18 +31,3 @@ propExprEval e (NonEmpty xs) = eval f e == eval f (optimizeExpr e)
 
 propExprOptimizeSmaller :: Expr -> Bool
 propExprOptimizeSmaller expr = exprComplexity expr >= exprComplexity (optimizeExpr expr)
-
-propExprListifyDepth :: Expr -> Bool
-propExprListifyDepth expr = l <= r
-  where
-    expr' = fromMaybe expr (listify expr)
-
-    l = case expr' of
-      Add a _ -> heigth a
-      Mul a _ -> heigth a
-      _       -> 0
-
-    r = case expr' of
-      Add _ b -> heigth b
-      Mul _ b -> heigth b
-      _       -> 0
