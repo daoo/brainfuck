4d7855583fb7cf95c6174d1692bb670356df29ce
daoo <daoo314@gmail.com>
2012-12-03 18:54:32 +0100

    Improve testing

--- haskell/tests/Properties/Optimization.hs
+++ haskell/tests/Properties/Optimization.hs
@@ -9,7 +9,7 @@ import Brainfuck.Optimization.Assignment
 import Brainfuck.Optimization.General
 import Brainfuck.Optimization.Pipeline
 import Data.ListZipper
-import Data.Sequence (empty)
+import Data.Sequence
 import Data.Word
 import Test.QuickCheck hiding (output)
 
@@ -28,14 +28,32 @@ compareFull i (State _ out1 m1) (State _ out2 m2) =
 compareOutput :: (Integral a) => State a -> State a -> Bool
 compareOutput s1 s2 = output s1 == output s2
 
-testCode :: (State Word8 -> State Word8 -> Bool) -> AST -> AST -> Bool
-testCode f ast ast' = f (run state ast) (run state ast')
+data TestResult = Correct | Incorrect
+  { failCode1 :: AST
+  , failCode2 :: AST
+  , failMemory1 :: ListZipper Word8
+  , failMemory2 :: ListZipper Word8
+  , failOutput1 :: Seq Word8
+  , failOutput2 :: Seq Word8
+  }
+
+wasCorrect :: TestResult -> Bool
+wasCorrect Correct = True
+wasCorrect _       = False
+
+testCode :: (State Word8 -> State Word8 -> Bool) -> AST -> AST -> TestResult
+testCode f ast ast' = if f state1 state2
+  then Correct
+  else Incorrect ast ast' (memory state1) (memory state2) (output state1) (output state2)
   where
+    state1 = run state ast
+    state2 = run state ast'
+
     state :: State Word8
     state = State [1..] empty newMemory
 
 propTransform :: (AST -> AST) -> AST -> Bool
-propTransform f ast = testCode (compareFull s) ast ast'
+propTransform f ast = wasCorrect $ testCode (compareFull s) ast ast'
   where
     ast' = f ast
 
@@ -56,4 +74,4 @@ propOptimizeMoveShifts  = propTransform moveShifts . getAst
 propOptimizeSets        = propTransform optimizeSets . getAst
 
 propOptimizeAll :: PrettyAST -> Bool
-propOptimizeAll (PrettyAST ast) = testCode compareOutput ast (optimizeAll ast)
+propOptimizeAll (PrettyAST ast) = wasCorrect $ testCode compareOutput ast (optimizeAll ast)
