2e1f08416fd7bb84cd341bc4693347fa5b280509
daoo <daoo314@gmail.com>
2013-02-04 08:53:17 +0100

    Try being more strict
    Stack overflow problems, needs more research.

--- haskell/src/Brainfuck/Optimization/Rule.hs
+++ haskell/src/Brainfuck/Optimization/Rule.hs
@@ -3,6 +3,7 @@ module Brainfuck.Optimization.Rule where
 
 import Brainfuck.Data.Expr
 import Control.Applicative
+import Data.List
 import Data.Maybe
 
 try :: (Monad f, Alternative f) => (a -> f a) -> f a -> f a
@@ -17,15 +18,21 @@ class Ruled a where
   descend :: (a -> Rule a) -> a -> Rule a
 
 rules :: Ruled a => [a -> Rule a] -> a -> Rule a
-rules fs e = foldl (\acc f -> descend f (fromMaybe e acc) <|> acc) empty fs
+rules fs e = foldl' (\acc f -> descend f (fromMaybe e acc) <|> acc) empty fs
 
 loop :: Ruled a => [a -> Rule a] -> a -> Rule a
 loop fs a = try (loop fs) (rules fs a)
 
 instance Ruled Expr where
   descend f e = case e of
-    Value _         -> Nothing
-    UnaryOp op a    -> try f $ pure $ UnaryOp op (df a)
-    BinaryOp op a b -> try f $ pure $ BinaryOp op (df a) (df b)
-    where
-      df = tryMaybe (descend f)
+    Value _ -> Nothing
+
+    UnaryOp op a -> case descend f a of
+      Nothing -> f e
+      Just a' -> f (UnaryOp op a')
+
+    BinaryOp op a b -> case (descend f a, descend f b) of
+      (Nothing, Nothing) -> f e
+      (Just a', Nothing) -> f $ BinaryOp op a' b
+      (Nothing, Just b') -> f $ BinaryOp op a b'
+      (Just a', Just b') -> f $ BinaryOp op a' b'
