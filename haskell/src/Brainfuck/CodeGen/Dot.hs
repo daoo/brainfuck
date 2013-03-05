91efdcd6233ee3c7197da9c125a8dfa02db99a51
daoo <daoo314@gmail.com>
2013-03-05 16:49:05 +0100

    Clean up and improve Dot CodeGen

--- haskell/src/Brainfuck/CodeGen/Dot.hs
+++ haskell/src/Brainfuck/CodeGen/Dot.hs
@@ -6,48 +6,58 @@ import Brainfuck.Data.Expr
 import Control.Monad.State
 import Text.CodeWriter
 
-type DotState a = StateT Int CodeWriter a
+type Id = Int
 
-newId :: DotState Int
+type DotState a = StateT Id CodeWriter a
+
+data Outline = Box | Ellipse | Diamond
+
+instance Show Outline where
+  show = \case
+    Box     -> "shape=\"box\""
+    Ellipse -> "shape=\"ellipse\""
+    Diamond -> "shape=\"diamond\""
+
+newId :: DotState Id
 newId = modify (+1) >> get
 
-node :: Int -> String -> DotState ()
-node n label = lift $ lineM $ do
+makeNode :: Outline -> Id -> String -> DotState ()
+makeNode outline n label = lift $ lineM $ do
   string (show n)
   string " [label=\""
   string label
-  string "\"];"
+  string "\" "
+  string (show outline)
+  string "];"
 
-edge :: Int -> Int -> String -> DotState ()
-edge from to color = lift $ lineM $ do
+makeEdge :: Id -> Id -> DotState ()
+makeEdge from to = lift $ lineM $ do
   string (show from)
   string " -> "
   string (show to)
-  string " [color=\""
-  string color
-  string "\"];"
+  string ";"
 
 showExpr :: Expr -> DotState ()
 showExpr = \case
   Value v -> do
     n <- get
-    node n (value v)
+    makeNode Ellipse n (value v)
 
   OperateUnary op a -> do
     n <- get
-    node n (unop op)
+    makeNode Ellipse n (unop op)
     next n a
 
   OperateBinary op a b -> do
     n <- get
-    node n (binop op)
+    makeNode Ellipse n (binop op)
     next n a
     next n b
 
   where
     next n e = do
       ne <- newId
-      edge n ne "black"
+      makeEdge n ne
       showExpr e
 
     value = \case
@@ -69,41 +79,41 @@ showAST ast = writeCode $ do
   line "}"
 
   where
-    go :: AST -> StateT Int CodeWriter ()
+    go :: AST -> DotState ()
     go = \case
       Nop -> do
         n <- get
-        node n "Nop"
+        makeNode Box n "Nop"
 
       Instruction fun next -> do
         n <- get
         case fun of
-          Set d e   -> exprNode n (showString "Set " $ show d) e
-          Shift i   -> node n (showString "Shift " $ show i)
-          PutChar e -> exprNode n "PutChar" e
-          GetChar d -> node n (showString "GetChar " $ show d)
+          Set d e   -> exprNode Box n (showString "Set " $ show d) e
+          Shift i   -> makeNode Box n (showString "Shift " $ show i)
+          PutChar e -> exprNode Box n "PutChar" e
+          GetChar d -> makeNode Box n (showString "GetChar " $ show d)
 
-        nextNode n next "black"
+        nextNode n next
 
       Flow ctrl inner next -> do
         n <- get
         case ctrl of
-          Forever -> node n "Forever"
-          Once    -> node n "Once"
-          Never   -> node n "Never"
-          If e    -> exprNode n "If" e
-          While e -> exprNode n "While" e
+          Forever -> makeNode Diamond n "Forever"
+          Once    -> makeNode Diamond n "Once"
+          Never   -> makeNode Diamond n "Never"
+          If e    -> exprNode Diamond n "If" e
+          While e -> exprNode Diamond n "While" e
 
-        nextNode n inner "blue"
-        nextNode n next "black"
+        nextNode n inner
+        nextNode n next
 
-    nextNode n ast' color = do
+    nextNode n ast' = do
       n' <- newId
-      edge n n' color
+      makeEdge n n'
       go ast'
 
-    exprNode n l e = do
-      node n l
+    exprNode outline n l expr = do
+      makeNode outline n l
       ne <- newId
-      edge n ne "red"
-      showExpr e
+      makeEdge n ne
+      showExpr expr
