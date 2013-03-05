4c1c4384b610af3cfff97e78dad89c3e2f3365ba
daoo <daoo314@gmail.com>
2013-03-05 09:36:28 +0100

    Add graph helper script

---
+++ haskell/tools/graph.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+bf="dist/build/brainfuck/brainfuck"
+dir="/tmp/brainfuck"
+
+infile="$1"
+
+if [[ ! -x "$bf" ]]; then
+  echo "brainfuck compiler not found"
+elif [[ ! -f "$infile" ]]; then
+  echo "usage: compile-and-run FILE"
+else
+  mkdir -p "$dir"
+
+  name="$(basename "$infile" .bf)"
+
+  dotfile="$dir/$name.dot"
+  svgfile="$dir/$name.svg"
+
+  $bf -c -t Dot "$infile" > "$dotfile"
+  dot "$dotfile" -Tsvg -o"$svgfile"
+fi
