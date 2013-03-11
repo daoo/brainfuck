a58c06e428be42c36812212c077c944e610b78d4
daoo <daoo314@gmail.com>
2013-03-11 10:02:08 +0100

    Work on extraction

--- haskell/coq/Extraction.v
+++ haskell/coq/Extraction.v
@@ -1,4 +1,33 @@
-Require Import Data.Expr Data.AST.
+Require Import Expr ssreflect ssrint.
 Extraction Language Haskell.
 
-Extraction "Expr.hs".
+Extract Inductive unit => "()" [ "()" ].
+Extract Inductive bool => "Bool" [ "True" "False" ].
+Extract Inductive sumbool => "Bool" [ "True" "False" ].
+Extract Inductive list => "List" [ "[]" "(:)" ].
+Extract Inductive option => "Maybe" [ "Just" "Nothing" ].
+Extract Inductive prod => "(,)" [ "(,)" ].
+Extract Inductive int => "Int" ["id" "-"]
+  "(\fPos fNeg n -> if n >= 0 then fPos n else fNeg n)".
+
+Extract Inductive Datatypes.nat => "Int" ["0" "succ"]
+  "(\fO fS n -> if n==0 then fO () else fS (n-1))".
+
+Extract Constant orb => "(||)".
+Extract Constant andb => "(&&)".
+Extract Constant negb => "not".
+Extraction Inline orb negb andb.
+
+Extract Inlined Constant Datatypes.length => "length".
+Extract Inlined Constant Coq.Lists.List.filter => "filter".
+Extract Inlined Constant app => "(++)".
+Extract Inlined Constant List.map => "map".
+
+Extract Constant option_rect => "flip maybe".
+Extraction Inline option_rect option_rec.
+
+Extract Constant fst => "fst".
+Extract Constant snd => "snd".
+Extraction Inline fst snd.
+
+Extraction "Expr.hs" unfold inlineExpr modifyValues eval.
