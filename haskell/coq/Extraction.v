951cb4713b46162731f5a2c1a1462468a48f2f97
daoo <daoo314@gmail.com>
2013-03-11 10:21:59 +0100

    Work on Extraction

--- haskell/coq/Extraction.v
+++ haskell/coq/Extraction.v
@@ -1,33 +1,22 @@
-Require Import Expr ssreflect ssrint.
+Require Import Expr ssreflect ssrint seq.
 Extraction Language Haskell.
 
 Extract Inductive unit => "()" [ "()" ].
-Extract Inductive bool => "Bool" [ "True" "False" ].
-Extract Inductive sumbool => "Bool" [ "True" "False" ].
-Extract Inductive list => "List" [ "[]" "(:)" ].
-Extract Inductive option => "Maybe" [ "Just" "Nothing" ].
-Extract Inductive prod => "(,)" [ "(,)" ].
-Extract Inductive int => "Int" ["id" "-"]
-  "(\fPos fNeg n -> if n >= 0 then fPos n else fNeg n)".
 
-Extract Inductive Datatypes.nat => "Int" ["0" "succ"]
-  "(\fO fS n -> if n==0 then fO () else fS (n-1))".
-
-Extract Constant orb => "(||)".
-Extract Constant andb => "(&&)".
-Extract Constant negb => "not".
+Extract Inductive bool => "Prelude.Bool" [ "Prelude.True" "Prelude.False" ].
+Extract Inductive sumbool => "Prelude.Bool" [ "Prelude.True" "Prelude.False" ].
+Extract Constant orb => "(Prelude.||)".
+Extract Constant andb => "(Prelude.&&)".
+Extract Constant negb => "Prelude.not".
 Extraction Inline orb negb andb.
 
-Extract Inlined Constant Datatypes.length => "length".
-Extract Inlined Constant Coq.Lists.List.filter => "filter".
-Extract Inlined Constant app => "(++)".
-Extract Inlined Constant List.map => "map".
-
-Extract Constant option_rect => "flip maybe".
+Extract Inductive option => "Prelude.Maybe" [ "Prelude.Just" "Prelude.Nothing" ].
+Extract Constant option_rect => "Prelude.flip Prelude.maybe".
 Extraction Inline option_rect option_rec.
 
-Extract Constant fst => "fst".
-Extract Constant snd => "snd".
+Extract Inductive prod => "(,)" [ "(,)" ].
+Extract Constant fst => "Prelude.fst".
+Extract Constant snd => "Prelude.snd".
 Extraction Inline fst snd.
 
 Extraction "Expr.hs" unfold inlineExpr modifyValues eval.
