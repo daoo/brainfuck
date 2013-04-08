bba12208e0b0544880340698907f69599ad3f29a
daoo <daoo314@gmail.com>
2013-04-08 14:14:10 +0200

    Clean up old code

--- haskell/src/Brainfuck/Data/Tarpit.hs
+++ haskell/src/Brainfuck/Data/Tarpit.hs
@@ -1,5 +1,8 @@
-{-# LANGUAGE LambdaCase #-}
-module Brainfuck.Data.Tarpit where
+module Brainfuck.Data.Tarpit
+  ( Function (..)
+  , Control (..)
+  , Tarpit (..)
+  ) where
 
 import Brainfuck.Data.Expr
 import Data.Monoid
@@ -22,24 +25,3 @@ instance Monoid Tarpit where
     Nop                  -> b
     Instruction fun next -> Instruction fun (mappend next b)
     Flow ctrl inner next -> Flow ctrl inner (mappend next b)
-
-initTarpit :: Tarpit -> Tarpit
-initTarpit = \case
-  Nop               -> Nop
-  Instruction _ Nop -> Nop
-  Flow _ Nop _      -> Nop
-  Flow _ _ Nop      -> Nop
-
-  Instruction fun next -> Instruction fun (initTarpit next)
-  Flow ctrl inner next -> Flow ctrl (initTarpit inner) (initTarpit next)
-
-initsTarpit :: Tarpit -> [Tarpit]
-initsTarpit = \case
-  Nop -> []
-  x   -> let x' = initTarpit x in x' : initsTarpit x'
-
-mapTarpit :: (Function -> Function) -> (Control -> Control) -> Tarpit -> Tarpit
-mapTarpit f g = \case
-  Nop                  -> Nop
-  Flow ctrl inner next -> Flow (g ctrl) (mapTarpit f g inner) (mapTarpit f g next)
-  Instruction fun next -> Instruction (f fun) (mapTarpit f g next)
