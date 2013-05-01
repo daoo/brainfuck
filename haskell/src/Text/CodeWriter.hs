bc7f491f23093702da5c528e598730a3108eb931
daoo <daoo314@gmail.com>
2013-05-01 10:05:26 +0200

    Force some inlining

--- haskell/src/Text/CodeWriter.hs
+++ haskell/src/Text/CodeWriter.hs
@@ -27,12 +27,15 @@ import qualified Data.ByteString.Lazy.Char8 as BS
 newtype CodeWriter a = CodeWriter { runCodeWriter :: String -> (a, String, Builder) }
 
 instance Monad CodeWriter where
+  {-# INLINE return #-}
   return x = CodeWriter $ \ind -> (x, ind, mempty)
 
+  {-# INLINE (>>=) #-}
   m >>= f = CodeWriter $ \ind -> case runCodeWriter m ind of
     (a, ind', build) -> case runCodeWriter (f a) ind' of
       (b, ind'', build') -> (b, ind'', build `mappend` build')
 
+  {-# INLINE (>>) #-}
   m1 >> m2 = CodeWriter $ \ind -> case runCodeWriter m1 ind of
     (_, ind', build) -> case runCodeWriter m2 ind' of
       (b, ind'', build') -> (b, ind'', build `mappend` build')
@@ -40,50 +43,63 @@ instance Monad CodeWriter where
   fail = error
 
 instance MonadState String CodeWriter where
+  {-# INLINE get #-}
+  {-# INLINE put #-}
+  {-# INLINE state #-}
   get     = CodeWriter $ \ind -> (ind, ind, mempty)
   put new = CodeWriter $ const ((), new, mempty)
   state f = CodeWriter $ \ind -> case f ind of (a, ind') -> (a, ind', mempty)
 
 instance MonadWriter Builder CodeWriter where
+  {-# INLINE writer #-}
+  {-# INLINE tell #-}
   writer (a, w) = CodeWriter $ \ind -> (a, ind, w)
   tell w        = CodeWriter $ \ind -> ((), ind, w)
 
+  {-# INLINE listen #-}
   listen f = do
     x <- f
     return (x, mempty)
 
+  {-# INLINE pass #-}
   pass f = CodeWriter $ \ind -> case runCodeWriter f ind of
     ((a, g), ind', build) -> (a, ind', g build)
 
+{-# INLINE incIndent #-}
+{-# INLINE decIndent #-}
 incIndent, decIndent :: CodeWriter ()
 incIndent = modify ((:) ' ' . (:) ' ')
 decIndent = modify (drop 2)
+
+{-# INLINE char #-}
+char :: Char -> CodeWriter ()
+char = tell . charUtf8
+
+{-# INLINE int #-}
+int :: Int -> CodeWriter ()
+int = tell . intDec
+
+{-# INLINE newline #-}
+newline :: CodeWriter ()
+newline = char '\n'
+
+{-# INLINE string #-}
+string :: String -> CodeWriter ()
+string = tell . stringUtf8
+
+indent :: CodeWriter ()
+indent = get >>= string
 
 lineM, indentedM :: CodeWriter () -> CodeWriter ()
 lineM m     = indent >> m >> newline
 indentedM m = incIndent >> m >> decIndent
 
-indent :: CodeWriter ()
-indent = get >>= string
-
-char :: Char -> CodeWriter ()
-char = tell . charUtf8
-
-int :: Int -> CodeWriter ()
-int = tell . intDec
+line :: String -> CodeWriter ()
+line = lineM . string
 
 surround :: Char -> Char -> Bool -> CodeWriter () -> CodeWriter ()
 surround a b True inner  = char a >> inner >> char b
 surround _ _ False inner = inner
-
-newline :: CodeWriter ()
-newline = char '\n'
-
-string :: String -> CodeWriter ()
-string = tell . stringUtf8
-
-line :: String -> CodeWriter ()
-line = lineM . string
 
 writeCode :: CodeWriter () -> Builder
 writeCode = thrd . (`runCodeWriter` "")
