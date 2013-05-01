79653345c34323af544d6f6bd0ed178600d19772
daoo <daoo314@gmail.com>
2013-05-01 09:55:15 +0200

    Clean up haskell code gen

--- haskell/src/Brainfuck/CodeGen/Haskell.hs
+++ haskell/src/Brainfuck/CodeGen/Haskell.hs
@@ -7,15 +7,8 @@ import Text.CodeWriter
 
 writeHaskell :: Tarpit -> CodeWriter ()
 writeHaskell code = do
-  line "import Brainfuck.Data.Expr hiding (eval)"
+  line "import Brainfuck.Data.Expr"
   line "import Brainfuck.Data.IOMemory"
-  line "import Control.Monad hiding (when)"
-  line "import Control.Monad.Trans"
-  line "import Data.Char"
-  line "import Prelude hiding (read)"
-  line ""
-  line "put = lift . putChar . chr"
-  line "get = ord `fmap` getChar"
   line ""
   line "main :: IO ()"
   line "main = runMemory 30001 $ do"
@@ -32,15 +25,14 @@ writeHaskell code = do
       While e -> block "while" e
 
     function = \case
-      Assign d e -> string "write " >> safeint d >> string " " >> expr e
-      PutChar e  -> string "put " >> expr e
-      GetChar d  -> string "write " >> safeint d >> string " =<< get"
-      Shift d    -> string "shift " >> safeint d
+      Assign d (Const c) -> string "write "                >> safeint d       >> string " "   >> safeint c
+      Assign d e         -> string "set "                  >> safeint d       >> string " $ " >> string (show e)
+      PutChar (Const c)  -> string "lift $ putChar $ chr " >> safeint c
+      PutChar e          -> string "putchr $ "             >> string (show e)
+      GetChar d          -> string "getchr "               >> safeint d
+      Shift d            -> string "shift "                >> safeint d
 
     safeint d = surround '(' ')' (d < 0) (int d)
-
-    expr (Const c) = int c
-    expr e         = string "=<< eval (" >> string (shows e ")")
 
     block str e = lineM $ do
       string str
--- haskell/src/Brainfuck/Data/IOMemory.hs
+++ haskell/src/Brainfuck/Data/IOMemory.hs
@@ -1,41 +1,63 @@
+{-# LANGUAGE BangPatterns #-}
 module Brainfuck.Data.IOMemory
-  ( read
+  ( runMemory
   , write
+  , putchr
+  , getchr
+  , set
   , shift
-  , eval
   , when
   , while
-  , runMemory
   ) where
 
 import Brainfuck.Data.Expr hiding (eval)
 import Control.Applicative hiding (Const)
-import Control.Monad.State.Strict hiding (when)
+import Control.Monad hiding (when)
 import Data.Array.IO
+import Data.Char
 import Prelude hiding (read)
+import qualified Control.Monad.State.Strict as S
 
 data Memory = Memory { ptr :: Int, array :: IOArray Int Int }
 
-type IOMemory a = StateT Memory IO a
+type IOMemory a = S.StateT Memory IO a
 
+{-# INLINE newMemory #-}
 newMemory :: Int -> IO Memory
 newMemory i = Memory <$> pure 0 <*> newArray (0, i) 0
 
+{-# INLINE read #-}
 read :: Int -> IOMemory Int
-read d = get >>= (\m -> lift $ readArray (array m) (ptr m + d))
+read d = S.get >>= (\m -> S.lift $ readArray (array m) (ptr m + d))
 
+{-# INLINE write #-}
 write :: Int -> Int -> IOMemory ()
-write d v = get >>= (\m -> lift $ writeArray (array m) (ptr m + d) v)
+write d v = S.get >>= (\m -> S.lift $ writeArray (array m) (ptr m + d) v)
 
-shift :: Int -> IOMemory ()
-shift d = modify (\m -> m { ptr = ptr m + d })
-
+{-# INLINE eval #-}
 eval :: Expr -> IOMemory Int
 eval = go 0
   where
-    go acc (Const c)    = return $ acc + c
-    go acc (Var n d xs) = (*n) <$> read d >>= (\acc' -> go (acc + acc') xs)
+    go !acc (Const c)    = return $ acc + c
+    go !acc (Var n d xs) = (*n) <$> read d >>= (\acc' -> go (acc + acc') xs)
 
+{-# INLINE putchr #-}
+putchr :: Expr -> IOMemory ()
+putchr e = eval e >>= S.lift . putChar . chr
+
+{-# INLINE getchr #-}
+getchr :: Int -> IOMemory ()
+getchr d = ord <$> S.lift getChar >>= write d
+
+{-# INLINE set #-}
+set :: Int -> Expr -> IOMemory ()
+set d e = eval e >>= write d
+
+{-# INLINE shift #-}
+shift :: Int -> IOMemory ()
+shift d = S.modify (\m -> m { ptr = ptr m + d })
+
+{-# INLINE when #-}
 when :: Expr -> IOMemory () -> IOMemory ()
 when e f = do
   x <- eval e
@@ -44,5 +66,6 @@ when e f = do
 while :: Expr -> IOMemory () -> IOMemory ()
 while e f = when e (f >> while e f)
 
+{-# INLINE runMemory #-}
 runMemory :: Int -> IOMemory () -> IO ()
-runMemory size m = newMemory size >>= (void . runStateT m)
+runMemory size m = newMemory size >>= (void . S.runStateT m)
