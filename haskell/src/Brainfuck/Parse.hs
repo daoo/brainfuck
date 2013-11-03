1c3d1e8dcaba71e2ab25e5011f925aafc6cba924
daoo <daoo314@gmail.com>
2013-11-03 19:39:15 +0100

    Clean up

--- haskell/src/Brainfuck/Parse.hs
+++ haskell/src/Brainfuck/Parse.hs
@@ -4,22 +4,21 @@ import Brainfuck.Data.Brainfuck
 import Control.Applicative
 import qualified Data.ByteString.Char8 as BS
 
-parse :: BS.ByteString -> (BS.ByteString, Brainfuck)
-parse bs = maybe (BS.empty, Nop) f (BS.uncons bs)
-  where
-    f (x, xs) = case x of
-      '+' -> Token Plus       <$> parse xs
-      '-' -> Token Minus      <$> parse xs
-      '>' -> Token ShiftRight <$> parse xs
-      '<' -> Token ShiftLeft  <$> parse xs
-      '.' -> Token Output     <$> parse xs
-      ',' -> Token Input      <$> parse xs
-
-      '[' -> let (xs', inner) = parse xs
-              in Repeat inner <$> parse xs'
-      ']' -> (xs, Nop)
-
-      _ -> parse xs
-
 parseBrainfuck :: BS.ByteString -> Brainfuck
-parseBrainfuck = snd . parse
+parseBrainfuck = snd . go
+  where
+    go bs = case BS.uncons bs of
+      Nothing      -> (BS.empty, Nop)
+      Just (x, xs) -> case x of
+        '+' -> Token Plus       <$> go xs
+        '-' -> Token Minus      <$> go xs
+        '>' -> Token ShiftRight <$> go xs
+        '<' -> Token ShiftLeft  <$> go xs
+        '.' -> Token Output     <$> go xs
+        ',' -> Token Input      <$> go xs
+
+        '[' -> let (xs', inner) = go xs
+                in Repeat inner <$> go xs'
+        ']' -> (xs, Nop)
+
+        _ -> go xs
