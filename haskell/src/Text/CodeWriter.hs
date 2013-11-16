1ab7bdcd837e7deb9e45620733beb6fab6efaf4a
daoo <daoo314@gmail.com>
2013-11-17 00:07:44 +0100

    Remove unnecessary state and writer monad instances

--- haskell/src/Text/CodeWriter.hs
+++ haskell/src/Text/CodeWriter.hs
@@ -18,8 +18,6 @@ module Text.CodeWriter
   ) where
 
 import Brainfuck.Utility
-import Control.Monad.State.Class
-import Control.Monad.Writer.Class
 import Data.ByteString.Builder
 import Data.Monoid
 import qualified Data.ByteString.Lazy.Char8 as BS
@@ -33,43 +31,32 @@ instance Monad CodeWriter where
   {-# INLINE (>>=) #-}
   m >>= f = CodeWriter $ \ind -> case runCodeWriter m ind of
     (a, ind', build) -> case runCodeWriter (f a) ind' of
-      (b, ind'', build') -> (b, ind'', build `mappend` build')
+      (b, ind'', build') -> (b, ind'', build <> build')
 
   {-# INLINE (>>) #-}
   m1 >> m2 = CodeWriter $ \ind -> case runCodeWriter m1 ind of
     (_, ind', build) -> case runCodeWriter m2 ind' of
-      (b, ind'', build') -> (b, ind'', build `mappend` build')
+      (b, ind'', build') -> (b, ind'', build <> build')
 
   fail = error
 
-instance MonadState String CodeWriter where
-  {-# INLINE get #-}
-  {-# INLINE put #-}
-  {-# INLINE state #-}
-  get     = CodeWriter $ \ind -> (ind, ind, mempty)
-  put new = CodeWriter $ const ((), new, mempty)
-  state f = CodeWriter $ \ind -> case f ind of (a, ind') -> (a, ind', mempty)
+{-# INLINE getIndent #-}
+getIndent :: CodeWriter String
+getIndent = CodeWriter $ \ind -> (ind, ind, mempty)
 
-instance MonadWriter Builder CodeWriter where
-  {-# INLINE writer #-}
-  {-# INLINE tell #-}
-  writer (a, w) = CodeWriter $ \ind -> (a, ind, w)
-  tell w        = CodeWriter $ \ind -> ((), ind, w)
+{-# INLINE modIndent #-}
+modIndent :: (String -> String) -> CodeWriter ()
+modIndent f = CodeWriter $ \ind -> ((), f ind, mempty)
 
-  {-# INLINE listen #-}
-  listen f = do
-    x <- f
-    return (x, mempty)
-
-  {-# INLINE pass #-}
-  pass f = CodeWriter $ \ind -> case runCodeWriter f ind of
-    ((a, g), ind', build) -> (a, ind', g build)
+{-# INLINE tell #-}
+tell :: Builder -> CodeWriter ()
+tell w = CodeWriter $ \ind -> ((), ind, w)
 
 {-# INLINE incIndent #-}
 {-# INLINE decIndent #-}
 incIndent, decIndent :: CodeWriter ()
-incIndent = modify ((:) ' ' . (:) ' ')
-decIndent = modify (drop 2)
+incIndent = modIndent ((:) ' ' . (:) ' ')
+decIndent = modIndent (drop 2)
 
 {-# INLINE char #-}
 char :: Char -> CodeWriter ()
@@ -87,8 +74,9 @@ newline = char '\n'
 string :: String -> CodeWriter ()
 string = tell . stringUtf8
 
+{-# INLINE indent #-}
 indent :: CodeWriter ()
-indent = get >>= string
+indent = getIndent >>= string
 
 lineM, indentedM :: CodeWriter () -> CodeWriter ()
 lineM m     = indent >> m >> newline
